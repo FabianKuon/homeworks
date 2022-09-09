9259b23391306b68fe78d996b48d95ef784d4efa
Fabian Kuon <kuonfabian@freenet.de>
2022-09-09 14:41:48 +0200

    antother try to fix tests

--- homeworks/homework_2/fortune_teller/fortune_teller.cpp
+++ homeworks/homework_2/fortune_teller/fortune_teller.cpp
@@ -12,9 +12,9 @@ int main() {
                                    {"summer", "C++ expert"},
                                    {"autumn", "coding beast"},
                                    {"winter", "software design hero"}};
-  string endings[] = {{"eats UB for breakfast"},
-                      {"finds errors quicker than the compiler"},
-                      {"is not afraid of C++ error messages"}};
+  string endings[] = {"eats UB for breakfast",
+                      "finds errors quicker than the compiler\'",
+                      "is not afraid of C++ error messages"};
 
   cout << "Welcome to the fortune teller program!" << endl;
   cout << "Please enter your name:" << endl;
@@ -28,9 +28,9 @@ int main() {
   cout << "Please enter another adjective:" << endl;
   cin >> adjectives[1];
 
-  printf("%s, the %s %s that %s\n", name.c_str(),
+  printf("%s, the %s %s that %s", name.c_str(),
          adjectives[name.length() % adjectives.size()].c_str(),
          noun.find(birthdayMonth)->second.c_str(),
-         endings[name.length() % endings->length()].c_str());
+         endings[name.length() % endings->size()].c_str());
   return 0;
 }
