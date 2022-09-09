040cdf5594020b0456b8d020e6935cfc40e9cf2b
Fabian Kuon <kuonfabian@freenet.de>
2022-09-09 14:47:03 +0200

    removed unnecessary sign

--- homeworks/homework_2/fortune_teller/fortune_teller.cpp
+++ homeworks/homework_2/fortune_teller/fortune_teller.cpp
@@ -13,7 +13,7 @@ int main() {
                                    {"autumn", "coding beast"},
                                    {"winter", "software design hero"}};
   string endings[] = {"eats UB for breakfast",
-                      "finds errors quicker than the compiler",
+                      "finds errors quicker than the compiler\'",
                       "is not afraid of C++ error messages"};
 
   cout << "Welcome to the fortune teller program!" << endl;
@@ -28,7 +28,7 @@ int main() {
   cout << "Please enter another adjective:" << endl;
   cin >> adjectives[1];
 
-  printf("%s, the %s %s that %s", name.c_str(),
+  printf("%s, the %s %s that %s\n", name.c_str(),
          adjectives[name.length() % adjectives.size()].c_str(),
          noun.find(birthdayMonth)->second.c_str(),
          endings[name.length() % endings->size()].c_str());
