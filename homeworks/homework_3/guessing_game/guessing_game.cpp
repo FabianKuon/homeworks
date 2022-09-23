8d78cf0b2ff8e008f7147526e3d3764e5de15749
Fabian Kuon <kuonfabian@freenet.de>
2022-09-23 10:32:30 +0200

    Solution to the guessing game

---
+++ homeworks/homework_3/guessing_game/guessing_game.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+int main() {
+  cout << "Welcome to the GUESSING GAME!" << endl;
+  cout << "I will generate a number and you will guess it!" << endl;
+  int min_number, max_number;
+  cout << "Please provide the smallest number: " << endl;
+  cin >> min_number;
+  cout << "Please provide the largest number: " << endl;
+  cin >> max_number;
+
+  random_device random_device;
+  mt19937 random_engine{random_device()};
+  uniform_int_distribution<> distribution{min_number, max_number};
+  int rand = distribution(random_engine);
+  cout << "I've generated a number. Try to guess it!" << endl;
+
+  int guess;
+  while (true) {
+    cout << "Please provide the next guess:" << endl;
+    cin >> guess;
+    if (guess < rand) {
+      cout << "Your number is too small. Try again!" << endl;
+    } else if (guess > rand) {
+      cout << "Your number is too big. Try again!" << endl;
+    } else {
+      break;
+    }
+  }
+
+  return 0;
+}
